3ef27aec2d0eeaf6710aa0a1dc5389806bd92b07
Suntiparp Somsak <sunnysomsak@gmail.com>
2026-03-10 12:58:25 +0000

    Add packages needed for API data download. Add script for downloading data from WorldPop FTP and NASA Earth.

--- 01_import-and-cleaning_2026.03.02.R
+++ 01_import-and-cleaning_2026.03.02.R
@@ -6,22 +6,46 @@
 ########################################################
 
 # Reset environment -----------------------------------------------------
-rm(list = ls())
-setwd("C:/Users/HP/Documents/GitHub/datharm-placement")
+  
+  rm(list = ls())
+  setwd("C:/Users/HP/Documents/GitHub/datharm-placement")
+  
+  #For downloading from WorldPop FTP
+  install.packages("devtools")
+  devtools::install_github("wpgp/wpgpDownloadR")
+  #For downloading from NASA Earth Data
+  install.packages("earthdatalogin")
+  devtools::install_github("boettiger-lab/earthdatalogin")
+  #For downloading from ACLED
+  install.packages("remotes")
+  remotes::install_gitlab("dickoa/racled")
+  
+  #For working with rasters
+  install.packages("sf")
+  install.packages("terra")
+  
+  library(devtools)
+  library(wpgpDownloadR)
+  library(earthdatalogin)
+  library(remotes)
+  library(sf)
+  library(terra)
+  
+  #Routine Packages
+  library(janitor)
+  library(dplyr)
+  library(tidyr)
+  library(ggplot2)
+  library(readr)
+  library(readxl)
 
-library(janitor)
-library(dplyr)
-library(tidyr)
-library(ggplot2)
-library(readr)
-library(readxl)
 #----------------------------------------------------------------------------
   
 ###################
 # Import Database # 
 ###################
 
-# Import tabs from sample MCHTrack dataset
+# 1. Import tabs from sample MCHTrack dataset ----------------------------------
 
   mch_linelisted <- read_xlsx("02_data/01_mchtrack-sample.xlsx", 
                                sheet = "SampleLinelisted") %>%
@@ -62,6 +86,39 @@ library(readxl)
     clean_names()
 
 
+# 2. Download WorldPop Data ----------------------------------------------------
+
+  #Explore WorldPop Data
+  wpgpListCountries()  
+  
+  #List all WorldPop covariate rasters for Nigeria
+  nga_datasets <- wpgpListCountryDatasets(ISO3 = "NGA")
+  head(nga_datasets[, c("CvtName", "Description")])
+  
+# 3. Download NASA Data ----------------------------------------------------
+  
+  #Get user and login details
+  edl_netrc()
+  
+  #Vector of IMERG file URLs for your period of interest (from GES DISC search)
+  url_nasa <- ("https://gpm1.gesdisc.eosdis.nasa.gov/data/GPM_L3/GPM_3IMERGM.07/")
+  
+  #Read Nigeria states and keep Katsina & Kano
+  nga_states <- st_read("path/to/nigeria_states.gpkg")
+  states  <- nga_states[nga_states$STATE_NAME %in% c("Katsina", "Kano"), ]
+
+  #Loop over IMERG files: open remotely, crop, summarize
+  results <- lapply(url_nasa, function(u) {
+    r <- rast(u, vsi = TRUE)               # open IMERG as SpatRaster
+    r <- rast(r, 1)                        # pick the precipitation variable/band
+    kk <- st_transform(c("Katsina", "Kano"), crs(r))
+    r_kk <- mask(crop(r, vect(kk)), vect(kk))
+    stats <- extract(r_kk, vect(kk), fun = mean, na.rm = TRUE)
+    data.frame(date = as.Date(get_z(r)),   # depends on how time is stored in IMERG file
+               state = kk$STATE_NAME,
+               pr   = stats$layer)
+  })
+  
 ############
 # Analysis # 
 ############
